94705dcc5ebeed937479c74e0ca58ba346b4046d
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2025-01-21 15:41:41 +0700

    add example for barcode processing from pick image

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 0.0.1-beta
 
 * Initial Release
+
+## 0.0.1
+
+* Add raw processing barcode detection for pick image feature
--- example/lib/presentation/main_page.dart
+++ example/lib/presentation/main_page.dart
@@ -1,7 +1,10 @@
 import 'package:example/data/dto/model/feature_model.dart';
 import 'package:example/presentation/barcode_scanner_page.dart';
+import 'package:example/presentation/preview_barcode_page.dart';
 import 'package:example/presentation/widget/feature_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feature_barcode/flutter_feature_barcode.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -11,18 +14,28 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final picker = ImagePicker();
+  final featureBarcode = FlutterFeatureBarcode();
+
   List<FeatureModel> features = [
     FeatureModel(
       title: 'Barcode Scanner',
       desc: 'Barcode Scanner',
       key: 'BARCODE_SCANNER',
     ),
+    FeatureModel(
+      title: 'Pick Image',
+      desc: 'Pick Image',
+      key: 'PICK_IMAGE',
+    ),
   ];
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {});
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      featureBarcode.initialize();
+    });
   }
 
   @override
@@ -39,6 +52,21 @@ class _MainPageState extends State<MainPage> {
               switch (feature.key) {
                 case "BARCODE_SCANNER":
                   Navigator.of(context).push(MaterialPageRoute(builder: (_) => const BarcodeScannerPage()));
+                  break;
+                case "PICK_IMAGE":
+                  picker.pickImage(source: ImageSource.gallery).then((xFile) {
+                    if (xFile != null) {
+                      featureBarcode.process(InputImage.fromFilePath(xFile.path)).then((barcodes) {
+                        if (barcodes.isNotEmpty) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (_) => PreviewBarcodePage(barcodeRawValue: barcodes.first.rawValue!)));
+                        } else {
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(content: Text('No barcode detected')));
+                        }
+                      });
+                    }
+                  });
                   break;
               }
             },
